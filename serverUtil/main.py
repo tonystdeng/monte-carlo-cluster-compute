05918654f8ff6054e9ba6b19e48e9e07f506f623
tonystdeng <tony.st.deng@gmail.com>
2026-04-07 12:01:28 -0400

    continued development on server utils and uploaded a visualization

---
+++ serverUtil/main.py
@@ -0,0 +1,30 @@
+# distrobute compute.py sftp
+# intereact with heap.py's info function for parameters
+# ssh into cluster computers and fun the cluster program
+# collect the console output of cluster program
+# give that output mentioned above to the orgnize funciton in head.py
+# give the orgnized data into the saveJSON function and get a json file
+# save the json file which marks the end of the comput session
+
+
+import paramiko
+
+# Open a transport
+host,port = "debian-node14",22
+transport = paramiko.Transport((host,port))
+
+# Auth    
+username,password = "node14","psw"
+transport.connect(None,username,password)
+
+# Go!    
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+
+filepath = "/home/foo.jpg"
+localpath = "/home/pony.jpg"
+sftp.put(localpath,filepath,)
+
+# Close
+if sftp: sftp.close()
+if transport: transport.close()
