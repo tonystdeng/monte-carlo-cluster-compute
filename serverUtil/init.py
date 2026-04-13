94d6d2798fb0779402204784ec74d87a5aa92cf2
tonystdeng <tony.st.deng@gmail.com>
2026-04-13 09:27:56 -0400

    commented init.py

--- serverUtil/init.py
+++ serverUtil/init.py
@@ -3,23 +3,27 @@ import os
 
 import clusterInfo as info
 
+# create the ssh client that will be used throughout the automation
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+# loop through all clusters
 for i in range(info.clusterNum):
+    # prepare based on index
     client.connect(hostname=info.hostnames[i], username=info.usernames[i], password=info.passwords[i])
-
     homePath = "/home/" + str(info.usernames[i])
 
+    # delete old version of cluster main folder and then install new ones
+    stdin, stdout, stderr = client.exec_command(f"rm -rf {homePath}/{info.mainFolderName}")
     stdin, stdout, stderr = client.exec_command(f"mkdir -p {homePath}/{info.mainFolderName}/{info.programsFolderName}")
-    print(stdout.read().decode())
-    print(stderr.read().decode())
 
+    # transfer cluster.py and its nessesary assist programs
     sftp = client.open_sftp()
     sftp.put(os.path.dirname(os.path.abspath(__file__)) + "/cluster.py", 
              homePath + f"/{info.mainFolderName}/{info.clusterProgramName}")
     sftp.put(os.path.dirname(os.path.abspath(__file__)) + "/clusterInfo.py", 
              homePath + f"/{info.mainFolderName}/clusterInfo.py")
+    
+    # end
     sftp.close()
-
     client.close()
